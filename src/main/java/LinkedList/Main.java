c279553b522ede8dde2150e38e547898d604aea6
prateekchhibber <0392pc@gmail.com>
2023-10-21 16:51:27 +0530

    Remove first item in LinkedList

--- src/main/java/LinkedList/Main.java
+++ src/main/java/LinkedList/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(2);
-        myLinkedList.append(3);
-        myLinkedList.prepend(1);
+        myLinkedList.append(1);
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeFirst());
         myLinkedList.printList();
     }
 }
