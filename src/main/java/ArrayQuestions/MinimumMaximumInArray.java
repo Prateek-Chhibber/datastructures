ea97820590ced467298f0ac5f983083e8334e682
prateekchhibber <0392pc@gmail.com>
2023-10-29 14:48:42 +0530

    Added more questions

--- src/main/java/ArrayQuestions/MinimumMaximumInArray.java
+++ src/main/java/ArrayQuestions/MinimumMaximumInArray.java
@@ -1,5 +1,12 @@
 package ArrayQuestions;
 
-public class MinimumMaximumInArray {
+import java.util.Arrays;
 
+public class MinimumMaximumInArray {
+    public static void main(String[] args) {
+        int[] myList = {5, 3, 8, 1, 6, 9};
+        int min = myList[0];
+        System.out.println(Arrays.stream(myList).max().toString());
+        System.out.println(Arrays.toString(myList));
+    }
 }
--- src/main/java/LinkedList/Main.java
+++ src/main/java/LinkedList/Main.java
@@ -7,25 +7,13 @@ public class Main {
         myLinkedList.append(2);
         myLinkedList.append(3);
         myLinkedList.append(4);
-        myLinkedList.append(5);
 
-        System.out.println("LL before remove():");
+        System.out.println("LL before reverse():");
         myLinkedList.printList();
 
-        System.out.println("\nRemoved node:");
-        System.out.println(myLinkedList.remove(2).value);
-        System.out.println("LL after remove() in middle:");
-        myLinkedList.printList();
+        myLinkedList.reverse();
 
-        System.out.println("\nRemoved node:");
-        System.out.println(myLinkedList.remove(0).value);
-        System.out.println("LL after remove() of first node:");
+        System.out.println("\nLL after reverse():");
         myLinkedList.printList();
-
-        System.out.println("\nRemoved node:");
-        System.out.println(myLinkedList.remove(2).value);
-        System.out.println("LL after remove() of last node:");
-        myLinkedList.printList();
-
     }
 }
