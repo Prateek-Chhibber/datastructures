6586c5d2a36b164945af5a946a3c44ba32d83d22
prateekchhibber <0392pc@gmail.com>
2023-11-04 16:49:49 +0530

    Consumer Functional Interface - Part 2

--- src/main/java/functionalInterfaces/ConsumerExample.java
+++ src/main/java/functionalInterfaces/ConsumerExample.java
@@ -1,13 +1,43 @@
 package functionalInterfaces;
 
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
 
+    static Consumer<Student> c2 = (student) -> System.out.println(student);
+    static Consumer<Student> c3 = (student) -> System.out.println(student.getName());
+    static Consumer<Student> c4 = (student) -> System.out.println(student.getActivities());
+
+    public static void printName(){
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(c2);
+    }
+
+    public static void printNameAndActivities(){
+        System.out.println("printNameAndActivities");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(c3.andThen(c4)); //Consumer chaining
+    }
+
+    public static void printNameAndActivitiesUsingCondition(){
+        System.out.println("printNameAndActivitiesUsingCondition");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach((student -> {
+            if (student.getGradeLevel()>=3 && student.getGpa()>=3.9){
+                c3.andThen(c4).accept(student);
+            }
+        })); //Consumer chaining
+    }
+
     public static void main(String[] args) {
-
         Consumer<String> c1 = (s) -> {System.out.println(s.toUpperCase());};
-
-        c1.accept("java8");
+//        c1.accept("java8");
+//        printName();
+        printNameAndActivities();
+        printNameAndActivitiesUsingCondition();
     }
 }
