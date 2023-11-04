9056c23cd95abd25bbbdf08d51895b79a28f07fc
prateekchhibber <0392pc@gmail.com>
2023-11-04 15:47:30 +0530

    Implement Runnable using Lambda

---
+++ src/main/java/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,33 @@
+package lambdas;
+
+public class RunnableLambdaExample {
+    public static void main(String[] args) {
+        /**
+         * Prior Java 8
+         */
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable 1");
+            }
+        };
+
+        new Thread(runnable).start();
+
+        //Java 8 - Lambda Syntax
+
+        // ()->{}
+        //assigning a lambda to a variable.
+        Runnable runnableLambda = () -> {
+            System.out.println("Inside Runnable 2");
+        };
+
+        Runnable runnableLambda1 = () -> {System.out.println("Inside Runnable 3");};
+
+        new Thread(runnableLambda).start();
+
+        new Thread(runnableLambda1).start();
+
+        new Thread(() -> {System.out.println("Inside Runnable 4");}).start();
+    }
+}
