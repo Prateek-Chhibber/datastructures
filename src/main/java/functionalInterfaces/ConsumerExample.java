5a87d099c6d1cfdbbac223be0129f8ce94772639
prateekchhibber <0392pc@gmail.com>
2023-11-04 19:29:41 +0530

    BiConsumer Functional Interface

--- src/main/java/functionalInterfaces/ConsumerExample.java
+++ src/main/java/functionalInterfaces/ConsumerExample.java
@@ -36,7 +36,7 @@ public class ConsumerExample {
     public static void main(String[] args) {
         Consumer<String> c1 = (s) -> {System.out.println(s.toUpperCase());};
 //        c1.accept("java8");
-//        printName();
+        printName();
         printNameAndActivities();
         printNameAndActivitiesUsingCondition();
     }
