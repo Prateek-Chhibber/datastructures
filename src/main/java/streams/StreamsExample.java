3ef970e12e66ef57553bd40a2788bb3c93a5e42c
prateekchhibber <0392pc@gmail.com>
2023-11-18 00:19:21 +0530

    Introduction to Streams

---
+++ src/main/java/streams/StreamsExample.java
@@ -0,0 +1,25 @@
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamsExample {
+
+    public static void main(String[] args) {
+        //    Student name and there activities in a map
+        Predicate<Student> studentPredicate = (student -> student.getGradeLevel()>=3);
+        Predicate<Student> studentgpaPredicate = (student -> student.getGpa()>=3.9);
+
+        Map<String, List> studentMap = StudentDataBase.getAllStudents().parallelStream()
+                .filter(studentPredicate)
+                .filter(studentgpaPredicate)
+                .collect(Collectors.toMap(Student::getName,Student::getActivities));
+        System.out.println(studentMap);
+    }
+
+}
